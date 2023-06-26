4e3eae223147508caccbc63a21cf1745fca52634
Daniel Marthan <danielmarthan@gmail.com>
2023-06-26 10:23:35 +0300

    built stacks

--- 05/main.rs
+++ 05/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs;
 
-// https://adventofcode.com/2022/day/4
+// https://adventofcode.com/2022/day/5
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.len() {
@@ -15,10 +15,37 @@ fn main() {
                 // let moves = file.next().unwrap();
 
                 let boxes = boxes.rsplit("\r\n");
+                let mut items = Vec::<char>::new();
                 for row in boxes {
-                    row.chars().collect::<Vec<char>>().chunks(4).for_each(|chunk| println!("{:?}", chunk));
-                    
+                    items.append(
+                        &mut row
+                            .chars()
+                            .collect::<Vec<char>>()
+                            .chunks(4)
+                            .map(|chunk| chunk[1])
+                            .collect::<Vec<char>>(),
+                    );
                 }
+
+                let mut stacks: Vec<Vec<char>> = Vec::new();
+                let mut stacks_number = 0;
+                let mut stacks_total = 0;
+                for (i, item) in items.into_iter().enumerate() {
+                    if item.is_numeric() {
+                        stacks_total += 1;
+                    } else {
+                        if stacks_number < stacks_total {
+                            let mut v: Vec<char> = Vec::new();
+                            v.push(item);
+                            stacks.push(v);
+                            stacks_number += 1;
+                        } else if item != ' ' {
+                            stacks[i % stacks_total].push(item);
+                        }
+                    }
+                }
+
+                println!("{:?}", stacks);
             } else {
                 println!("Invalid file name: {}", &args[1]);
             }
