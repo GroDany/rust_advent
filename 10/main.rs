769f7ab40718b5ff312c75801fd4f3bfdcc836a4
Daniel <danielmarthan@gmail.com>
2023-07-03 19:24:44 +0300

    10 p1

--- 10/main.rs
+++ 10/main.rs
@@ -10,6 +10,36 @@ fn main() {
         }
         2 => {
             if let Ok(file) = fs::read_to_string(&args[1]) {
+                let file = file.split("\n");
+                let mut cycle = 1;
+                let mut x = 1;
+                let mut cache = 0;
+                let mut strength = 0;
+
+                for line in file {
+                    if cache != 0 {
+                        x += cache;
+                    }
+                    if line.is_empty() { break; }
+                    let line = line.split(" ").collect::<Vec<&str>>();
+                    match line[0] {
+                        "addx" => {
+                            cache = line[1].parse::<i32>().unwrap();
+                            if cycle % 40 == 20 {
+                                strength += x * cycle;
+                                // println!("{} {}: {}", x, cycle, x * cycle);
+                            }
+                            cycle += 1;
+                        },
+                        _ => cache = 0,
+                    }
+                    if cycle % 40 == 20 {
+                        strength += x * cycle;
+                        // println!("{} {}: {}", x, cycle, x * cycle);
+                    }
+                    cycle += 1;
+                }
+                println!("Solution is: {}", strength);
             } else {
                 println!("Invalid file name: {}", &args[1]);
             }
