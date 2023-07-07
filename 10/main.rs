29818e7e6f85c76cc715be7e5590e95028d04d9f
Daniel <danielmarthan@gmail.com>
2023-07-07 20:07:01 +0300

    10 p2

--- 10/main.rs
+++ 10/main.rs
@@ -13,30 +13,34 @@ fn main() {
                 let file = file.split("\n");
                 let mut cycle = 1;
                 let mut x = 1;
-                let mut cache = 0;
 
                 for line in file {
-                    if cache != 0 {
-                        x += cache;
+                    if line.is_empty() {
+                        break;
                     }
-                    if line.is_empty() { break; }
                     let line = line.split(" ").collect::<Vec<&str>>();
-                    match line[0] {
-                        "addx" => {
-                            cache = line[1].parse::<i32>().unwrap();
-                            if cycle % 40 == 0 {
-                                print!("\n");
-                            }
-                            cycle += 1;
-                        },
-                        _ => cache = 0,
+                    let acc = if line[0] == "addx" {
+                       2
+                    } else {
+                        1 
+                    };
+                    for n in 0..acc {
+                        if x - 1 <= (cycle - 1) % 40 && (cycle - 1) % 40 <= x + 1 {
+                            print!("#");
+                        } else {
+                            print!(".");
+                        }
+                        if cycle % 40 == 0 {
+                            print!("\n");
+                        }
+                        cycle += 1;
+                        if n == 1 {
+                            x += line[1].parse::<i32>().unwrap();
+                        }
                     }
-                    if cycle % 40 == 0 {
-                        print!("\n");
-                    }
-                    cycle += 1;
                 }
-                println!("Solution is: {}", strength);
+
+                // cache = line[1].parse::<i32>().unwrap();
             } else {
                 println!("Invalid file name: {}", &args[1]);
             }
