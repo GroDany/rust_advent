10dcfe6826c4afe2b21d54b35d9aaa483e74d48b
Daniel <danielmarthan@gmail.com>
2023-07-08 13:22:17 +0300

    11 p2

--- 11/main.rs
+++ 11/main.rs
@@ -3,12 +3,12 @@ use std::fs;
 
 #[derive(Debug)]
 struct Monkey {
-    items: Vec<i32>,
+    items: Vec<u128>,
     operands: Vec<String>,
-    mult: i32,
+    mult: u128,
     true_monkey: usize,
     false_monkey: usize,
-    inspected: i32,
+    inspected: u128,
 }
 
 impl Monkey {
@@ -17,12 +17,12 @@ impl Monkey {
 
         let items = monkey[1].replace("  Starting items: ", "");
         let items = items.split(", ");
-        let items = items.map(|item| item.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+        let items = items.map(|item| item.parse::<u128>().unwrap()).collect::<Vec<u128>>();
 
         let operands = monkey[2].replace("  Operation: new = ", "");
         let operands = operands.split(" ").map(|s| String::from(s)).collect::<Vec<String>>();
 
-        let mult = monkey[3].replace("  Test: divisible by ", "").parse::<i32>().unwrap();
+        let mult = monkey[3].replace("  Test: divisible by ", "").parse::<u128>().unwrap();
         let tm = monkey[4].replace("    If true: throw to monkey ", "").parse::<usize>().unwrap();
         let fm = monkey[5].replace("    If false: throw to monkey ", "").parse::<usize>().unwrap();
 
@@ -36,30 +36,30 @@ impl Monkey {
         }
     }
 
-    fn operate(&self, item: i32) -> i32 {
+    fn operate(&self, item: u128, m: u128) -> u128 {
         let a = if self.operands[0] == "old" {
             item
         } else {
-            self.operands[0].parse::<i32>().unwrap()
+            self.operands[0].parse::<u128>().unwrap()
         };
         let b = if self.operands[2] == "old" {
             item
         } else {
-            self.operands[2].parse::<i32>().unwrap()
+            self.operands[2].parse::<u128>().unwrap()
         };
         match self.operands[1].as_str() {
-            "+" => a + b,
-            _ => a * b,
+            "+" => (a + b) % m,
+            _ => (a * b) % m,
         }
     }
 
-    fn process_item(&mut self) -> Option<(usize, i32)> {
+    fn process_item(&mut self, m: u128) -> Option<(usize, u128)> {
         if self.items.is_empty() {
             return None;
         }
         self.inspected += 1;
-        let item: i32 = self.items.remove(0);
-        let item = self.operate(item) / 3;
+        let item: u128 = self.items.remove(0);
+        let item = self.operate(item, m);
         if item % self.mult == 0 {
             Some((self.true_monkey, item))
         } else {
@@ -67,24 +67,24 @@ impl Monkey {
         }
     }
 
-    fn receive_item(&mut self, item: i32) {
+    fn receive_item(&mut self, item: u128) {
         self.items.push(item);
     }
 }
 
-fn process(monkeys: &mut Vec<Monkey>) {
+fn process(monkeys: &mut Vec<Monkey>, m: u128) {
     let len = monkeys.len();
 
-    for _ in 0..20 {
+    for _ in 0..10000 {
         for n in 0..len {
-            while let Some(target) = monkeys[n].process_item() {
+            while let Some(target) = monkeys[n].process_item(m) {
                 monkeys[target.0].receive_item(target.1);
             }
         }
     }
 }
 
-fn compute(monkeys: &Vec<Monkey>) -> i32 {
+fn compute(monkeys: &Vec<Monkey>) -> u128 {
     let mut m0 = monkeys[0].inspected;
     let mut m1 = 0;
     let len = monkeys.len();
@@ -98,6 +98,16 @@ fn compute(monkeys: &Vec<Monkey>) -> i32 {
         }
     }
     m0 * m1
+}
+
+fn compute_big_mult(monkeys: &Vec<Monkey>) -> u128 {
+    let mut mult = 1;
+    let len = monkeys.len();
+
+    for i in 0..len {
+        mult *= monkeys[i].mult;
+    }
+    mult
 }
 
 // https://adventofcode.com/2022/day/10
@@ -117,8 +127,9 @@ fn main() {
                     }
                     monkeys.push(Monkey::from(monkey_conf));
                 }
-                process(&mut monkeys);
-                dbg!(compute(&monkeys));
+                let m = compute_big_mult(&monkeys);
+                process(&mut monkeys, m);
+                println!("Solution is: {}", compute(&monkeys));
             } else {
                 println!("Invalid file name: {}", &args[1]);
             }
